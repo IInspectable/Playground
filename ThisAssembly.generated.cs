cfd7ae893398237d45af775e1bdb1287a9cf187e
IInspectable <github@smjh.de>
2021-01-01 23:34:00 +0100

    scripts added

---
+++ ThisAssembly.generated.cs
@@ -0,0 +1,7 @@
+// Warning: This is generated code! Don't touch as it will be overwritten by the code generator.
+namespace IInspectable.Utilities {
+    static partial class ThisAssembly {
+        public const string ProductVersion = "2.1.0";
+        public const string ProductName = "Playground";
+    }
+}
